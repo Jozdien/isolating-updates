474725b6dfa79b7e5f69f76781950397853d0c8c
Jozdien <jozdien@gmail.com>
2022-10-08 00:04:08 +0530

    New run, with much longer trainings.

--- analyze_run.py
+++ analyze_run.py
@@ -19,11 +19,10 @@ fuel_stds = utils.fuel_stds(rewards)
 fuel_variance = utils.fuel_variance(rewards)
 fuel_variance_no_zeros = utils.fuel_variance_no_zeros(rewards)
 
-print('Reward zeros percent: {}'.format(reward_zeros_percent))
 print('Reward means: {}'.format(reward_means))
 print('Reward standard deviations: {}'.format(reward_stds))
 print('Reward variances: {}'.format(reward_variance))
-print('Reward variances (no zeros): {}'.format(reward_variance_no_zeros))
+
 print('Fuel zeros percent: {}'.format(fuel_zeros_percent))
 print('Fuel means: {}'.format(fuel_means))
 print('Fuel standard deviations: {}'.format(fuel_stds))
